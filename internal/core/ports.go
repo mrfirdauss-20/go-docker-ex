817966410dc07ca58b02c66dd21f8d0069871051
Riandy Rahman Nugraha <riandyrn@gmail.com>
2022-04-02 20:04:48 +0300

    docs: add comments to ports for implementation guidance;

--- internal/core/ports.go
+++ internal/core/ports.go
@@ -6,18 +6,27 @@ import (
 )
 
 type GameStorage interface {
+	// PutGame is used for putting the game instance on storage. If game is already
+	// exists on storage, it will be overwritten.
 	PutGame(ctx context.Context, g Game) error
+
+	// GetGame returns game instance stored on storage. Returns nil when game is not
+	// found on storage.
 	GetGame(ctx context.Context, gameID string) (*Game, error)
 }
 
 type QuestionStorage interface {
+	// GetRandomQuestion returns random question from storage.
 	GetRandomQuestion(ctx context.Context) (*Question, error)
 }
 
 type Clock interface {
+	// Now returns current time.
 	Now() time.Time
 }
 
 type TimeoutCalculator interface {
+	// CalcTimeout returns proper duration to answer question for given number
+	// of correct answers.
 	CalcTimeout(ctx context.Context, countCorrect int) int
 }
