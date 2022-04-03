add02a2bc9907fc35c207b53a8946139b61908fd
Riandy Rahman Nugraha <riandyrn@gmail.com>
2022-04-03 22:11:51 +0300

    feat: finish progressbar;

--- cmd/mem_server/web/assets/app.js
+++ cmd/mem_server/web/assets/app.js
@@ -6,16 +6,27 @@ Vue.createApp({
             question: "1 + 2",
             choices: ["1", "2", "3"],
             score: 0,
-            timeout: 5,
-            correct_idx: 2
+            timeout: 1000, // 5 seconds
+            duration: 1000, // 5 seconds
+            correct_idx: 2,
+            questInterval: null
         }
     },
     methods: {
         play() {
             this.state = "NEW_QUESTION"
+            this.timeout = 1000;
+            this.duration = 1000;
+            this.questInterval = setInterval(() => {
+                this.duration-=1;
+                if (this.duration === 0) {
+                    this.submitAnswer(-1);
+                }
+            }, 5)
         },
         submitAnswer(idx) {
-            this.state = "GAME_OVER"
+            clearTimeout(this.questInterval);
+            this.state = "GAME_OVER";
         }
     }
 }).mount('#app')
