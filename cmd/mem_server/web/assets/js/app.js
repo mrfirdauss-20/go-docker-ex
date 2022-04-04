8f3dd6f6e62f56b2aaa32ab37908cd3746b6831d
Riandy Rahman Nugraha <riandyrn@gmail.com>
2022-04-04 09:53:50 +0300

    fix: fix submit answer logic & generate question logic;

--- cmd/mem_server/web/assets/js/app.js
+++ cmd/mem_server/web/assets/js/app.js
@@ -1,27 +1,79 @@
-Vue.createApp({
+const apiKey = "c4211664-47dc-4887-a2fe-9e694fbaf55a"
+
+function getNowUnix() {
+    return Math.floor(Date.now() / 1000);
+}
+
+const app = Vue.createApp({
     data() {
         return {
-            player_name: "Riandy",
-            state: "NEW_GAME",
-            question: "1 + 2",
-            choices: ["1", "2", "3"],
+            player_name: "",
+            game_id: "",
+            scenario: "NEW_GAME",
+            question: "",
+            choices: [],
             score: 0,
             max_duration: 0,
             duration: 0,
-            correct_idx: 2,
-            questInterval: null
+            correct_idx: -1,
+            questInterval: null,
+            start_at: 0
         }
     },
     methods: {
-        play() {
-            this.state = "NEW_QUESTION"
-            this.startTimer(5, () => {
-                this.submitAnswer(0);
+        async play() {
+            // start new game
+            let response = await fetch("/games", {
+                method: 'POST',
+                headers: { 'X-API-Key': apiKey, 'Content-Type': 'application/json' },
+                body: JSON.stringify({player_name: this.player_name})
+            });
+            let data = (await response.json()).data;
+            this.game_id = data.game_id;
+            this.fetchNewQuestion();
+        },
+        async fetchNewQuestion() {
+            // fetch new question
+            response = await fetch(`/games/${this.game_id}/question`, {
+                method: 'PUT',
+                headers: { 'X-API-Key': apiKey },
+            })
+            data = (await response.json()).data;
+            // update state
+            this.question = data.problem;
+            this.choices = data.choices;
+            this.start_at = getNowUnix();
+            this.scenario = data.scenario;
+            // start timer
+            this.startTimer(data.timeout, () => {
+                // sent no answer if timeout
+                this.submitAnswer(-1);
             });
         },
         async submitAnswer(idx) {
+            // stop timer
             this.stopTimer();
-            this.state = "GAME_OVER";
+            // submit answer
+            const answer_idx = idx + 1 // because according to api doc answer index start from 1
+            let response = await fetch(`/games/${this.game_id}/answer`, {
+                method: 'PUT',
+                headers: { 'X-API-Key': apiKey, 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    answer_idx: answer_idx,
+                    start_at: this.start_at,
+                    sent_at: getNowUnix(),
+                })
+            });
+            let data = (await response.json()).data;
+            console.log(data);
+            // update state
+            this.correct_idx = data.correct_idx - 1;
+            this.score = data.score;
+            if (data.scenario === 'NEW_QUESTION') {
+                this.fetchNewQuestion();
+                return
+            }
+            this.scenario = data.scenario;
         },
         startTimer(seconds, timeoutCallback) {
             // set current duration & max duration
@@ -39,6 +91,5 @@ Vue.createApp({
         stopTimer() {
             clearTimeout(this.questInterval);
         }
-
     }
-}).mount('#app')
+}).mount('#app');
