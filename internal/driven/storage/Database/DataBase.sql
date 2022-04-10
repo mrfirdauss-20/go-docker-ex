b3cf6386fdf32a2f3ecaf4f8fbdcc0289ab1857d
Muhammad Risqi Firdaus <69805783+mrfirdauss-20@users.noreply.github.com>
2022-04-10 09:47:01 +0700

    nambahin if exist

--- internal/driven/storage/Database/DataBase.sql
+++ internal/driven/storage/Database/DataBase.sql
@@ -1,15 +1,22 @@
+DROP DATABASE IF EXISTS hex_math;
+CREATE DATABASE hex_math;
+USE hex_math;
+
+DROP TABLE IF EXISTS Questions;
 CREATE TABLE `Questions` (
   `questionsId` int PRIMARY KEY,
   `Problem` varchar(255),
   `quantity` int DEFAULT 1
 );
 
-CREATE TABLE `choices` (
+DROP TABLE IF EXISTS choices;
+CREATE TABLE `Choices` (
   `QId` int,
   `choice` varchar(255),
   PRIMARY KEY (`QId`, `choice`)
 );
 
+DROP TABLE IF EXISTS Games;
 CREATE TABLE `Games` (
   `GamesId` int,
   `PlayerName` int UNIQUE,
