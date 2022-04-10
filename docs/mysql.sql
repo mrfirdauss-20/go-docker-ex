faede8f02704998a1cf1603e9b7f42f91303f2f7
Muhammad Risqi Firdaus <69805783+mrfirdauss-20@users.noreply.github.com>
2022-04-10 17:42:17 +0700

    fix drop statement and games table

--- docs/mysql.sql
+++ docs/mysql.sql
@@ -1,24 +1,21 @@
 CREATE DATABASE hex_math;
 USE hex_math;
 
-DROP TABLE IF EXISTS questions;
 CREATE TABLE `questions` (
   `id` int PRIMARY KEY,
   `problem` text,
   `correct_index` int
 );
 
-DROP TABLE IF EXISTS choices;
 CREATE TABLE `choices` (
   `question_id` int,
   `choice` varchar(255),
   PRIMARY KEY (`question_id`, `choice`)
 );
 
-DROP TABLE IF EXISTS games;
 CREATE TABLE `games` (
-  `id` int,
-  `player_name` int PRIMARY KEY,
+  `id` varchar(36)  PRIMARY KEY,
+  `player_name` int,
   `scenario` varchar(20) DEFAULT "NEW_QUESTION",
   `score` int,
   `count_correct` int,
