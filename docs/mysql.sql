80a7244f614d4d5b501baa36186e78abeba6eac6
Muhammad Risqi Firdaus <69805783+mrfirdauss-20@users.noreply.github.com>
2022-04-10 16:28:32 +0700

    fix variable naming to snake_case

--- docs/mysql.sql
+++ docs/mysql.sql
@@ -4,29 +4,29 @@ USE hex_math;
 DROP TABLE IF EXISTS questions;
 CREATE TABLE `questions` (
   `id` int PRIMARY KEY,
-  `Problem` text,
-  `correctIndex` int
+  `problem` text,
+  `correct_index` int
 );
 
 DROP TABLE IF EXISTS choices;
 CREATE TABLE `choices` (
-  `questionId` int,
+  `question_id` int,
   `choice` varchar(255),
-  PRIMARY KEY (`questionId`, `choice`)
+  PRIMARY KEY (`question_id`, `choice`)
 );
 
 DROP TABLE IF EXISTS games;
 CREATE TABLE `games` (
   `id` int,
-  `playerName` int UNIQUE,
+  `player_name` int PRIMARY KEY,
   `scenario` varchar(20) DEFAULT "NEW_QUESTION",
   `score` int,
-  `countCorrect` int,
-  `questionId` int,
-  `questionTimeout` int DEFAULT 5
+  `count_correct` int,
+  `question_id` int,
+  `question_timeout` int DEFAULT 5
 );
 
-ALTER TABLE `choices` ADD FOREIGN KEY (`questionId`) REFERENCES `questions` (`id`);
+ALTER TABLE `choices` ADD FOREIGN KEY (`question_id`) REFERENCES `questions` (`id`);
 
-ALTER TABLE `games` ADD FOREIGN KEY (`questionId`) REFERENCES `questions` (`iq`);
+ALTER TABLE `games` ADD FOREIGN KEY (`question_id`) REFERENCES `questions` (`id`);
 
