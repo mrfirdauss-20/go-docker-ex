0228692746372f5464d35020170e86880b90fe1c
Riandy Rahman Nugraha <riandyrn@gmail.com>
2022-04-22 18:08:15 +0300

    test: make test helper reusable;

--- internal/driven/storage/mysql/queststrg/storage_test.go
+++ internal/driven/storage/mysql/queststrg/storage_test.go
@@ -2,27 +2,20 @@ package queststrg_test
 
 import (
 	"context"
-	"encoding/json"
-	"fmt"
-	"os"
 	"testing"
 
 	"github.com/ghazlabs/hex-mathrush/internal/core"
 	"github.com/ghazlabs/hex-mathrush/internal/driven/storage/mysql/queststrg"
-	_ "github.com/go-sql-driver/mysql"
-	"github.com/jmoiron/sqlx"
-	"github.com/jmoiron/sqlx/types"
+	"github.com/ghazlabs/hex-mathrush/internal/driven/storage/mysql/testutils"
 	"github.com/stretchr/testify/require"
 )
 
-const envKeySQLTestDSN = "SQL_TEST_DSN"
-
 func TestGetRandomQuestion(t *testing.T) {
 	// initialize sql client
-	sqlClient, err := sqlx.Connect("mysql", os.Getenv(envKeySQLTestDSN))
+	sqlClient, err := testutils.InitSQLClient()
 	require.NoError(t, err)
 	// reset question table for clean slate
-	err = resetQuestionTable(sqlClient)
+	err = testutils.ResetQuestionTable(sqlClient)
 	require.NoError(t, err)
 	// insert several questions
 	questions := []core.Question{
@@ -37,7 +30,7 @@ func TestGetRandomQuestion(t *testing.T) {
 			CorrectIndex: 1,
 		},
 	}
-	err = insertQuestions(sqlClient, questions)
+	err = testutils.InsertQuestions(sqlClient, questions)
 	require.NoError(t, err)
 	// execute get random question
 	storage, err := queststrg.New(queststrg.Config{SQLClient: sqlClient})
@@ -46,45 +39,4 @@ func TestGetRandomQuestion(t *testing.T) {
 	require.NoError(t, err)
 	// check if returned question is from inserted questions
 	require.Contains(t, questions, *restQuestion)
-}
-
-func resetQuestionTable(sqlClient *sqlx.DB) error {
-	query := `SET FOREIGN_KEY_CHECKS=0; TRUNCATE TABLE questions;`
-	_, err := sqlClient.Exec(query)
-	if err != nil {
-		return fmt.Errorf("unable to execute query due: %w", err)
-	}
-	return nil
-}
-
-func insertQuestions(sqlClient *sqlx.DB, questions []core.Question) error {
-	var qRows []questionRow
-	for _, question := range questions {
-		qRows = append(qRows, newQuestionRow(question))
-	}
-	query := `
-		INSERT INTO questions (problem, correct_index, answers) VALUES (
-			:problem, :correct_index, :answers
-		)
-	`
-	_, err := sqlClient.NamedExec(query, qRows)
-	if err != nil {
-		return fmt.Errorf("unable to execute query due: %w", err)
-	}
-	return nil
-}
-
-type questionRow struct {
-	Problem      string         `db:"problem"`
-	CorrectIndex int            `db:"correct_index"`
-	Answers      types.JSONText `db:"answers"`
-}
-
-func newQuestionRow(q core.Question) questionRow {
-	b, _ := json.Marshal(q.Choices)
-	return questionRow{
-		Problem:      q.Problem,
-		CorrectIndex: q.CorrectIndex,
-		Answers:      types.JSONText(b),
-	}
 }
---
+++ internal/driven/storage/mysql/testutils/utils.go
@@ -0,0 +1,60 @@
+package testutils
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/ghazlabs/hex-mathrush/internal/core"
+	"github.com/jmoiron/sqlx"
+	"github.com/jmoiron/sqlx/types"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+const envKeySQLTestDSN = "SQL_TEST_DSN"
+
+func InitSQLClient() (*sqlx.DB, error) {
+	return sqlx.Connect("mysql", os.Getenv(envKeySQLTestDSN))
+}
+
+func ResetQuestionTable(sqlClient *sqlx.DB) error {
+	query := `SET FOREIGN_KEY_CHECKS=0; TRUNCATE TABLE questions;`
+	_, err := sqlClient.Exec(query)
+	if err != nil {
+		return fmt.Errorf("unable to execute query due: %w", err)
+	}
+	return nil
+}
+
+func InsertQuestions(sqlClient *sqlx.DB, questions []core.Question) error {
+	var qRows []questionRow
+	for _, question := range questions {
+		qRows = append(qRows, newQuestionRow(question))
+	}
+	query := `
+		INSERT INTO questions (problem, correct_index, answers) VALUES (
+			:problem, :correct_index, :answers
+		)
+	`
+	_, err := sqlClient.NamedExec(query, qRows)
+	if err != nil {
+		return fmt.Errorf("unable to execute query due: %w", err)
+	}
+	return nil
+}
+
+type questionRow struct {
+	Problem      string         `db:"problem"`
+	CorrectIndex int            `db:"correct_index"`
+	Answers      types.JSONText `db:"answers"`
+}
+
+func newQuestionRow(q core.Question) questionRow {
+	b, _ := json.Marshal(q.Choices)
+	return questionRow{
+		Problem:      q.Problem,
+		CorrectIndex: q.CorrectIndex,
+		Answers:      types.JSONText(b),
+	}
+}
