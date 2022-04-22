be5b6b2ac34bcf183eb6436df78d7eb8f9c71ab7
Riandy Rahman Nugraha <riandyrn@gmail.com>
2022-04-22 08:44:23 +0300

    docs: simplify mysql schema;

--- docs/mysql.sql
+++ docs/mysql.sql
@@ -4,18 +4,13 @@ USE hex_math;
 CREATE TABLE `questions` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
   `problem` text NOT NULL,
-  `correct_index` int NOT NULL
-);
-
-CREATE TABLE `choices` (
-  `question_id` int,
-  `choice` varchar(255),
-  PRIMARY KEY (`question_id`, `choice`)
+  `correct_index` int NOT NULL,
+  `answers` json NOT NULL
 );
 
 CREATE TABLE `games` (
   `id` varchar(36)  PRIMARY KEY,
-  `player_name` int NOT NULL,
+  `player_name` TEXT NOT NULL,
   `scenario` varchar(20) NOT NULL,
   `score` int NOT NULL,
   `count_correct` int NOT NULL,
@@ -23,7 +18,16 @@ CREATE TABLE `games` (
   `question_timeout` int NOT NULL
 );
 
-ALTER TABLE `choices` ADD FOREIGN KEY (`question_id`) REFERENCES `questions` (`id`);
-
 ALTER TABLE `games` ADD FOREIGN KEY (`question_id`) REFERENCES `questions` (`id`);
 
+INSERT INTO questions (problem, correct_index, answers) VALUES
+('1 + 1', 2, '[1, 2, 3]'),
+('1 + 2', 3, '[1, 2, 3]'),
+('2 + 1', 3, '[1, 2, 3]'),
+('3 - 2', 1, '[1, 2, 3]'),
+('2 - 1', 1, '[1, 2, 3]'),
+('2 + 2 - 1', 3, '[1, 2, 3]'),
+('1 + 1 + 1', 3, '[1, 2, 3]'),
+('3 - 1', 2, '[1, 2, 3]'),
+('2 + 1 - 2', 1, '[1, 2, 3]'),
+('1 + 1 - 1', 1, '[1, 2, 3]');
